c4ce6097fc9e6116340b055131d2309ef3800b14
JC Brand <jc@opkode.com>
2015-01-05 15:48:06 +0100

    Add main.js for paths config and add index.html

---
+++ main.js
@@ -0,0 +1,38 @@
+require.config({
+    baseUrl: "src",
+    paths: {
+        "i18n":                 "bower_components/patternslib/src/core/i18n",
+        "jquery":               "bower_components/jquery/jquery",
+        "logging":              "bower_components/logging/src/logging",
+        "pat-autosuggest":      "bower_components/patternslib/src/pat/autosuggest",
+        "pat-compat":           "bower_components/patternslib/src/core/compat",
+        "pat-jquery-ext":       "bower_components/patternslib/src/core/jquery-ext",
+        "pat-logger":           "bower_components/patternslib/src/core/logger",
+        "pat-parser":           "bower_components/patternslib/src/core/parser",
+        "pat-registry":         "bower_components/patternslib/src/core/registry",
+        "pat-utils":            "bower_components/patternslib/src/core/utils",
+        "patterns":             "bower_components/patternslib/bundle",
+        "picker":               "bower_components/pickadate/lib/picker",
+        "picker.date":          "bower_components/pickadate/lib/picker.date",
+        "picker.time":          "bower_components/pickadate/lib/picker.time",
+        "select2":              "bower_components/select2/select2"
+    },
+
+    "shim": {
+        "logging": { "exports": "logging" },
+    }
+});
+
+require(["pat-registry", "pat-pickadate"], function(registry, upload) {
+    window.patterns = registry;
+    // workaround this MSIE bug :
+    // https://dev.plone.org/plone/ticket/10894
+    if ($.browser.msie) {
+        $("#settings").remove();
+    }
+    window.Browser = {};
+    window.Browser.onUploadComplete = function () {};
+    registry.init();
+    return;
+});
+
