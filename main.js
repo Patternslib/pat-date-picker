df783fcfa120d0f2e85839ca4323238a7c3960ff
JC Brand <jc@opkode.com>
2015-02-24 11:02:41 +0100

    Use the mockup base pattern.
    This fixes various problems due to the difference in nature between Patternslib
    and Mockup patterns.
    
    Mockup instantiates a new pattern instance (with its own closure) for each DOM
    element, Patternslib calls the same pattern instance for each DOM element
    (effectively giving it a global closure for all DOM elements).
    
    Since this code is taken from Mockup, we need to adhere to the assumption that
    each pattern has its own closure, which we do by using the Mockup base pattern.

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ require.config({
     baseUrl: "src",
     paths: {
         "i18n":                 "bower_components/patternslib/src/core/i18n",
-        "jquery":               "bower_components/jquery/jquery",
+        "jquery":               "bower_components/jquery/dist/jquery",
+        "jquery.browser":       "bower_components/jquery.browser/dist/jquery.browser",
         "logging":              "bower_components/logging/src/logging",
         "pat-autosuggest":      "bower_components/patternslib/src/pat/autosuggest",
         "pat-compat":           "bower_components/patternslib/src/core/compat",
         "pat-jquery-ext":       "bower_components/patternslib/src/core/jquery-ext",
         "pat-logger":           "bower_components/patternslib/src/core/logger",
         "pat-parser":           "bower_components/patternslib/src/core/parser",
+        "mockup-parser":        "bower_components/mockup-core/js/parser",
+        "mockup-patterns-base": "bower_components/mockup-core/js/pattern",
         "pat-registry":         "bower_components/patternslib/src/core/registry",
         "pat-utils":            "bower_components/patternslib/src/core/utils",
         "patterns":             "bower_components/patternslib/bundle",
@@ -23,7 +26,7 @@ require.config({
     }
 });
 
-require(["pat-registry", "pat-pickadate"], function(registry, upload) {
+require(["pat-registry", "pat-pickadate", "jquery.browser"], function(registry, upload) {
     window.patterns = registry;
     // workaround this MSIE bug :
     // https://dev.plone.org/plone/ticket/10894
